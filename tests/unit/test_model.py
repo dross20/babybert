2ab3483d15405624a5247bd96f4f407fa37213c1
Drew Ross <drew.e.ross@gmail.com>
2025-08-09 16:07:19 -0500

    Add unit test

---
+++ tests/unit/test_model.py
@@ -0,0 +1,24 @@
+import torch
+
+from babybert.config import BabyBERTConfig
+from babybert.model import BabyBERT
+
+
+def test_forward_pass():
+    config = BabyBERTConfig(
+        vocab_size=5,
+        hidden_size=10,
+        block_size=5,
+        n_blocks=1,
+        n_heads=10,
+        sep_token_id=0,
+        attention_dropout_probability=0.1,
+        attention_projection_dropout_probability=0.1,
+        mlp_dropout_probability=0.1,
+        embedding_dropout_probability=0.1,
+    )
+
+    input = torch.asarray([[1, 2, 0, 4, 3], [1, 2, 0, 3, 4]])
+    model = BabyBERT(config)
+    output = model(input)
+    assert output.shape == (2, 5, 10)
